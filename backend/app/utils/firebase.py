9fa154dad63fd8dc92644e2b045f802d376f4fbb
tsbala777 <balaselvi544@gmail.com>
2026-01-04 12:11:58 +0530

    Fix : Deployment issue

--- backend/app/utils/firebase.py
+++ backend/app/utils/firebase.py
@@ -1,7 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
-cred = credentials.Certificate("firebase_key.json")
+FIREBASE_KEY_PATH = os.getenv("FIREBASE_KEY_PATH", "firebase_key.json")
+
+cred = credentials.Certificate(FIREBASE_KEY_PATH)
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
